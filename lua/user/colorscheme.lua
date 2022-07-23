79c185be0c41c73493fea3acf58f880d928b5b40
misku <miskuu@gmail.com>
2022-07-23 02:39:23 +0200

    Change the color scheme to a custom one
    misku/monokai.nvim

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,14 +1,15 @@
 local M = {
-  "folke/tokyonight.nvim",
-  commit = "e52c41314e83232840d6970e6b072f9fba242eb9",
+  "misku/monokai.nvim",
+  commit = "02d2ef4f73ba45d171ebdcbc716fbed2036eab16",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
-M.name = "tokyonight-night"
+M.name = "monokai"
 function M.config()
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
+    vim.notify("colorscheme " .. M.name .. " not found!")
     return
   end
 end
